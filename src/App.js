4e40fed0c824af7daed6816b35c93fc533f6a7db
sparrowml <ben@sparrow.dev>
2024-07-25 15:56:52 -0500

    basic chat ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
 function App() {
+  const [messages, setMessages] = React.useState([]);
+
+  const handleNewMessage = async (innerHtml, _, __) => {
+    setMessages([
+      ...messages,
+      {
+        message: innerHtml,
+        direction: "outgoing",
+      },
+      {
+        message: "...",
+        direction: "incoming",
+      },
+    ]);
+    const response = await axios.post("http://localhost:5000/openai/invoke", {
+      input: { input: innerHtml },
+    });
+    if (response.data.output?.output) {
+      setMessages([
+        ...messages,
+        {
+          message: innerHtml,
+          direction: "outgoing",
+        },
+        {
+          message: response.data.output.output,
+          direction: "incoming",
+        },
+      ]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div style={{ width: "500px", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message) => (
+                <Message model={message} key={message.message} />
+              ))}
+            </MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              attachButton={false}
+              onSend={handleNewMessage}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
